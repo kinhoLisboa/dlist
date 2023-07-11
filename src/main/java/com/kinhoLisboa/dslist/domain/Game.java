b680d065bd173c7ab08c121370aa0bc088aa3d3d
Danifleikson Buriti Lisboa <danifleikson@hotmail.com>
2023-07-10 22:16:26 -0300

    Criando o import sql e ajustando a entidade Game.

--- src/main/java/com/kinhoLisboa/dslist/domain/Game.java
+++ src/main/java/com/kinhoLisboa/dslist/domain/Game.java
@@ -1,9 +1,11 @@
 package com.kinhoLisboa.dslist.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -16,18 +18,25 @@ import lombok.Setter;
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
+@Table(name = "tb_game")
 public class Game {
 	
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String titlo;
-	private Integer ano;
-	private String genero;
-	private String plataforma;
+	private String title;
+	
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
-	private String shortDesciption;
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 }
