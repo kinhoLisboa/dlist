c52620ba9e30b88b4ff84a1bb8e2110a043b2acc
Danifleikson Buriti Lisboa <danifleikson@hotmail.com>
2023-07-15 11:13:26 -0300

    Adicionando um endpoint especial de mudança de posicionamento em lista.

---
+++ src/main/java/com/kinhoLisboa/dslist/dto/ReplacementDto.java
@@ -0,0 +1,11 @@
+package com.kinhoLisboa.dslist.dto;
+
+import lombok.Value;
+
+@Value
+public class ReplacementDto {
+	
+	private Integer sourceIndex;
+	private Integer destinationIndex;
+
+}
--- src/main/java/com/kinhoLisboa/dslist/service/GameListService.java
+++ src/main/java/com/kinhoLisboa/dslist/service/GameListService.java
@@ -6,18 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kinhoLisboa.dslist.dto.GameListDto;
+import com.kinhoLisboa.dslist.projection.GameMinProjection;
 import com.kinhoLisboa.dslist.repository.GameListRepository;
+import com.kinhoLisboa.dslist.repository.GameRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
+	@Autowired
+	private GameRepository gameRepository;
 	
+	@Transactional
 	public List<GameListDto> findAll(){
 		var result = gameListRepository.findAll();
 		
 		return result.stream().map( y -> new GameListDto(y)).toList();
 		}
+	@Transactional
+	public void move( Long listId,int sourceIndex, int destinatoionIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinatoionIndex,obj);
+		
+    	int min = sourceIndex < destinatoionIndex ? sourceIndex : destinatoionIndex;
+    	int max = sourceIndex < destinatoionIndex ? destinatoionIndex : sourceIndex;
+    	
+    	for(int i = min; i <= max; i++) {
+    		gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+    	}
+	}
 
 }
